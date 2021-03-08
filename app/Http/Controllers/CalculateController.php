45fbd0de73828341b07ef70bb3fb2cc4b07d473a
pfilipiak <patrykfilipiak@outlook.com>
2021-03-08 15:31:09 +0100

    init

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -19,11 +19,17 @@ class CalculateController extends Controller
         $rsi14 = collect(trader_rsi($data, 14))->values()->slice(-2,1)->last();
         $ema9 = collect(trader_ema($data, 9))->values()->slice(-2,1)->last();
         $ema26 = collect(trader_ema($data, 26))->values()->slice(-2,1)->last();
+        $ema5 = collect(trader_ema($data, 5))->values()->last();
+        $ema8 = collect(trader_ema($data, 8))->values()->last();
+        $ema13 = collect(trader_ema($data, 13))->values()->last();
 
         return response()->json([
             'rsi_14' => $rsi14,
             'ema_9' => $ema9,
-            'ema_26' => $ema26
+            'ema_26' => $ema26,
+            'ema_5' => $ema5,
+            'ema_8' => $ema8,
+            'ema_13' => $ema13,
         ]);
     }
 
